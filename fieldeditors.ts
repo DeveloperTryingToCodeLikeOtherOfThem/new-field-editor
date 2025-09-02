7789157299e0be1d388008867bdbc7fecfc7e0b9
a012 <Aeagan0507@gmail.com>
2025-09-01 21:54:56 -0400

    weight issues

--- fieldeditors.ts
+++ fieldeditors.ts
@@ -6,7 +6,7 @@ const canvas = new Image(); // for the new field editor
 
 namespace images {
     //% blockId=images_canvas block="canvas"
-    //% weight=95
+    //% weight=99
     export function canvas() {
         return canvas;
     }
