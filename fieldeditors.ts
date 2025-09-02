603241fd6798436baf44c858abfc07ca81537caf
a012 <Aeagan0507@gmail.com>
2025-09-01 21:59:16 -0400

    image

--- fieldeditors.ts
+++ fieldeditors.ts
@@ -2,12 +2,12 @@ class Image implements Image {
    constructor () { }
 }
 
-const canvas = new Image(); // for the new field editor
+// const canvas = new Image() // for the new field editor
 
 namespace images {
     //% blockId=images_canvas block="canvas"
     //% weight=99
-    export function canvas() {
-        return canvas;
+    export function canvas(image: Image): Image {
+        return image;
     }
 }
